e69db8a1fa47a0c6262ce6df06f674845cf49471
Мария Гаврилова <topik2344@gmail.com>
2023-02-19 21:01:58 +0800

    Добавляет стилизации

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,76 @@
 /* в этот файл добавляет скрипты*/
+
+/*Menu*/
+
+const onenMenu = () => {
+  let header = document.querySelector('.header');
+  let headerToggle = document.querySelector('.user-menu__toggle');
+
+  header.classList.remove('header--no-js');
+
+  headerToggle.addEventListener('click', function () {
+    if (header.classList.contains('header--closed')) {
+      header.classList.remove('header--closed');
+      headerToggle.classList.add('user-menu__toggle--close');
+    } else {
+      header.classList.add('header--closed');
+      headerToggle.classList.remove('user-menu__toggle--close');
+    }
+  });
+}
+
+onenMenu();
+
+/*Slider*/
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
+
+
+/*Map*/
+let mapSection = document.querySelector('.map');
+mapSection.classList.remove('map--no-js');
+
+const SHOP_LAT = 59.968266;
+const SHOP_LNG = 30.3174089;
+const ZOOM = 16;
+const TILELAYER_URL = 'https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=HMfyPUotkQhoWYIXN1mz';
+
+const map = L.map('map').setView([SHOP_LAT, SHOP_LNG], ZOOM);;
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.tileLayer(TILELAYER_URL, {
+  crossOrigin: true
+}).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: "../img/map-pin.svg",
+  iconSize: [39, 50],
+  iconAnchor: [18, 50],
+});
+
+const mainPin = L.marker(
+  {
+    lat: SHOP_LAT,
+    lng: SHOP_LNG,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainPin.addTo(map);
